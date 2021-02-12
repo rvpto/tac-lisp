fa7da91dffda7c74fc2f239f600e4764feed7f6e
Rui Valporto <ruivalporto@gmail.com>
2021-02-12 01:31:15 -0300

    Commit inicial

---
+++ tac.lisp
@@ -0,0 +1,36 @@
+; TAC (`cat` reverso)
+; rui valporto, 2021
+
+; função p/ obter arquivo
+; "nome do arquivo" => ("cada" "uma" "das linhas")
+;
+(defun linhas (nome-do-arquivo)
+  (with-open-file (stream nome-do-arquivo)
+    (loop for line = (read-line stream nil)
+          while line
+          collect line)))
+
+; função p/ reverter listas
+; 
+(defun R (L)
+  (do ((L2 () (cons (car L1) L2))
+       (L1 L (cdr L1)))
+      ((eq L1 ())
+       L2)))
+
+; função principal
+;
+(defun main ()
+  (let ((arquivo "")
+        (reverso ()))
+    
+;    (setq arquivo (cadr sb-ext:*posix-argv*))
+    (setq arquivo (nth 1 sb-ext:*posix-argv*))
+    (setq reverso (R (linhas arquivo)))
+
+    (loop for i in reverso
+          do (format t "~a ~%" i))))
+
+(sb-ext:save-lisp-and-die "tac"
+:executable t
+:toplevel #'main)
